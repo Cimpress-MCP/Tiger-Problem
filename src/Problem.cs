e7e14ee97329ac7a1ec07e850948093e633ba119
Christopher Osborn <cosborn@cimpress.com>
2017-09-26 11:17:47 -0400

    Allow Problem to Be Initialized with Extensions
    This should make conversion from an Error significantly more convenient.
    
    - (m) Add Copyright Notices Properly

--- src/Problem.cs
+++ src/Problem.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿// <copyright file="Problem.cs" company="Cimpress, Inc.">
+//   Copyright 2017 Cimpress, Inc.
+//
+//   Licensed under the Apache License, Version 2.0 (the "License");
+//   you may not use this file except in compliance with the License.
+//   You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//   Unless required by applicable law or agreed to in writing, software
+//   distributed under the License is distributed on an "AS IS" BASIS,
+//   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//   See the License for the specific language governing permissions and
+//   limitations under the License.
+// </copyright>
+
+using System;
 using System.Collections.Generic;
 using JetBrains.Annotations;
 using Newtonsoft.Json;
@@ -16,15 +32,24 @@ namespace Tiger.Problem
     {
         static readonly Uri s_blank = new Uri(@"about:blank", Absolute);
 
+        readonly Dictionary<string, dynamic> _extensions = new Dictionary<string, dynamic>(Ordinal);
+
         /// <summary>Initializes a new instance of the <see cref="Problem"/> class.</summary>
-        public Problem()
+        /// <param name="extensions">A collection of extension data members for this problem type.</param>
+        public Problem([CanBeNull] IDictionary<string, dynamic> extensions = null)
         {
+            if (extensions != null)
+            {
+                _extensions = new Dictionary<string, dynamic>(extensions, Ordinal);
+            }
         }
 
         /// <summary>Initializes a new instance of the <see cref="Problem"/> class.</summary>
         /// <param name="type">The identifier  of the problem type.</param>
+        /// <param name="extensions">A collection of extension data members for this problem type.</param>
         [JsonConstructor]
-        public Problem([CanBeNull] Uri type)
+        public Problem([CanBeNull] Uri type, [CanBeNull] IDictionary<string, dynamic> extensions = null)
+            : this(extensions)
         {
             Type = type ?? s_blank;
         }
@@ -53,18 +78,13 @@ namespace Tiger.Problem
         [JsonProperty(DefaultValueHandling = Ignore)]
         public Uri Instance { get; set; }
 
-        /// <summary>Gets a collection of extension data members for this problem type. </summary>
+        /// <summary>Gets a collection of extension data members for this problem type.</summary>
         /// <remarks>
         /// Essentially, any extension data members are namespaced by <see cref="Type"/>.
         /// Thus, they are "for this problem type", not "for this occurrence of the problem".
         /// </remarks>
-        /* note(cosborn)
-         * Due to a bug in Newtonsoft.Json, this property's keys will be serialized
-         * with the provided capitalization, not as specified in the startup.
-         */
         [JsonExtensionData, NotNull]
-        public IDictionary<string, dynamic> Extensions { get; } =
-            new Dictionary<string, dynamic>(Ordinal);
+        public IDictionary<string, dynamic> Extensions => _extensions;
 
         /// <summary>Determines whether <see cref="Type"/> should be serialized.</summary>
         /// <returns>
